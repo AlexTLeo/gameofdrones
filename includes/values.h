4c8588b2a76ff8558db84dc4b773b3d6b7d843cd
Thanaphon Leonardi <thanaphon.leonardi@gmail.com>
2022-01-04 16:38:05 +0100

    Update values.h

--- includes/values.h
+++ includes/values.h
@@ -24,3 +24,14 @@ const struct CoordinatePair START1 = {.x = 40, .y = 0};
 const struct CoordinatePair START2 = {.x = 0, .y = 80};
 const struct CoordinatePair START3 = {.x = 40, .y = 80};
 const struct CoordinatePair START4 = {.x = 20, .y = 40};
+
+/*
+Group 0: Michele Pestarino + Federico Sacco
+Group 1: Elea Papin + Paul Raingeard
+Group 2: Ebru Baglan + Tachadol Suthisomboon + Yusuke Kido
+Group 3: Antonio Bucciero + Marco Limone
+Group 4: Simone Contorno + Gabriele Russo
+Group 5: Alex Thanaphon Leonardi
+*/
+
+// REMEMBER TO SET SOCKOPT TO REUSE ADDRESS
