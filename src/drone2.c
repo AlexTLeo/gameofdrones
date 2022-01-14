68f5a89fc41620989dd62be10d6d05f81a155aaf
aob787 <aob787@gmail.com>
2022-01-14 15:21:38 +0100

    Milestone 2: random trajectory and communicate with master.

--- src/drone2.c
+++ src/drone2.c
@@ -8,9 +8,11 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/select.h>
+#include <time.h>
 #define GROUPNUM 2
 #define SA struct sockaddr
-
+#define MINSTEP 3
+#define MAXSTEP 3 //The real maxstep is maxstep + minstep
 void errorPrompt(int state,char error_msg[])
 {
   if (state < 0){
@@ -19,24 +21,91 @@ void errorPrompt(int state,char error_msg[])
   }
 }
 
-void sendAndReceive(int sockfd)
+void randomDir(int maxstep, int arr[])
 {
-  char returnMSG[256];
-  char *msg2server = "Hello, world from group 2";
+    srand(time(NULL));            // Initialization, should only be called once.
+    int newdir = rand() % 8;      // Returns a pseudo-random integer between 0 and RAND_MAX.
+    int n = rand() % maxstep + MINSTEP; // Returns a pseudo-random integer between 0 and RAND_MAX.
+    arr[0] = newdir;
+    arr[1] = n;
+}
+
+void position(int x, int y, int newdir, int coordinate[])
+{
+    switch (newdir)
+    {
+    case 0:
+        /* up */
+        x = x + 2;
+        break;
+    case 1:
+        // upperright
+        x = x + 2;
+        y = y + 2;
+        break;
+    case 2:
+        // right
+        y = y + 2;
+        break;
+    case 3:
+        // bottom right
+        x = x - 2;
+        y = y + 2;
+        break;
+    case 4:
+        // bottom
+        x = x - 2;
+        break;
+    case 5:
+        // bottom left
+        x = x - 2;
+        y = y - 2;
+        break;
+    case 6:
+        // left
+        y = y - 2;
+        break;
+    case 7:
+        // upper left
+        x = x + 2;
+        y = y - 2;
+        break;
+    default:
+        printf("random: something unexpected happened.\n");
+        break;
+    }
+
+    coordinate[0] = x;
+    coordinate[1] = y;
+}
+
+int sendAndReceive(int sockfd, int goTo [])
+{
+  int returnMSG;
   //msg2server = "Hello world from group 2";
   printf("Sending msg......");
   //write how many bytes that server should recieve -------
-  // ################################################
-  if (write(sockfd, msg2server, 256) < 0 ){
+  // Sending co-ordiante to master
+  if (write(sockfd, goTo, sizeof(int)*2) < 0 ){
     perror("sending failed...");
     exit(0);
   }
-  printf("Sucess\n");
-  if (read(sockfd, returnMSG, 255) < 0 ){
+  printf("Send successfully\n");
+  // Recieve the permission
+  if (read(sockfd, returnMSG, sizeof(int)*1) < 0 ){
     perror("reading failed...");
     exit(0);
   }
-  printf("From Server : %s\n", returnMSG);
+  printf("From Server : %d\n", returnMSG);
+
+  if(returnMSG == 0){
+    printf("Rejected by Master (rejected code: %d), unable to move to %d, %d", returnMSG, goTo[0], goTo[0]);
+    return 0;
+  }
+  if else (returnMSG == 1){
+      printf("Approved by Master (code: %d), moving to %d, %d", returnMSG, goTo[0], goTo[0]);
+      return 1;
+  }
 }
 
 int main()
@@ -70,15 +139,54 @@ int main()
   tv.tv_sec = 0;
   tv.tv_usec = TIMESTEP;
 
-  while (1){
-    errorPrompt(connect(sockfd, (SA*)&servaddr, sizeof(servaddr)), "connection with the server failed...");
-    printf("connected to the server..\n");
-    // function for chat
-    sendAndReceive(sockfd);
-    // close the socket
-    errorPrompt(close(sockfd), "close failed...");
+
+  int dir = 0;
+  int step = 0;
+  int res =0;
+  int x,y =0;
+  int coordinate[2], trajectory[2], goTo[2];
+  int fuel = 500;
+  //coordinate = values.h
+  coordinate[0] = 0;
+  coordinate[1] = 80;
+  trajectory[1] = 0;
+
+  while(1){
+    if (fuel == 0){
+      // Sending to master 
+      sleep(4);
+      fuel = 500;
+    }
+    if (trajectory[1]==0){
+      randomDir(MAXSTEP, trajectory);
+    }
+    position(coordinate[0], coordinate[1], trajectory[0], goTo);
+
+    for (i=0; i<= DRONE_TIMEOUT; i++){ //this one i <= Timeout
+      errorPrompt(connect(sockfd, (SA*)&servaddr, sizeof(servaddr)), "connection with the server failed...");
+      printf("connected to the server..\n");
+      sendAndReceive(sockfd, goTo);
+      res = Aob([x,y]);
+      errorPrompt(close(sockfd), "close failed...");
+      printf("Close successfully..\n");
+      if(res==1){
+        position(coordinate[0], coordinate[1], trajectory[0], coordinate);
+        fuel --;
+        trajectory[1] --;
+        break;
+      }
+      else{
+        printf("Permission denied (Attempt: %d)\n", i);
+        errorPrompt(select(1, NULL, NULL, NULL, &tv), "select failed...");
+      }
+    }
+
+    if(res==0){
+      printf("Timeout\n");
+      randomDir(MAXSTEP, trajectory);
+    }
     errorPrompt(select(1, NULL, NULL, NULL, &tv), "select failed...");
-    break;
     //Using select insead of sleep for preciser timing ref: https://stackoverflow.com/questions/3125645/why-use-select-instead-of-sleep
   }
+  return 0;
 }
